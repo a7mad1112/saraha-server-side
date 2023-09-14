450ea787ae25fb055b09370457c379d1fe467deb
a7mad1112 <ahmalawneh79@gmail.com>
2023-09-14 12:14:57 +0300

    login

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -11,8 +11,24 @@ export const signup = async (req, res) => {
   const hashedPassword = await bcryptjs.hash(password, +process.env.SALT_ROUND);
 
   const createUser = await userModel.create({
-    userName, email, password, gender, password: hashedPassword
-  })
+    userName,
+    email,
+    password,
+    gender,
+    password: hashedPassword,
+  });
 
-  return res.json({ msg: "success", user: createUser._id });
+  return res.json({ msg: 'success', user: createUser._id });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+  if (!user) return res.json({ msg: 'Invalid data' });
+
+  const match = bcryptjs.compareSync(password, user.password)
+  if (!match) return res.json({ msg: 'Invalid data' });
+
+  return res.json(user);
 };
--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -4,4 +4,5 @@ import * as authController from './auth.controller.js';
 const app = express();
 
 app.post('/signup', authController.signup);
+app.post('/login', authController.login);
 export default app;
