01a23506d3125d669caf8261e523ddf15059989d
a7mad1112 <ahmalawneh79@gmail.com>
2023-09-25 14:08:45 +0300

    installing cors

--- src/app.router.js
+++ src/app.router.js
@@ -2,8 +2,11 @@ import msgRouter from './modules/msg/msg.router.js';
 import authRouter from './modules/auth/auth.router.js';
 import userRouter from './modules/user/user.router.js'
 import connectDB from '../db/connection.js';
+import cors from 'cors'
 
 const initApp = (app, express) => {
+
+  app.use(cors())
   connectDB()
   app.use(express.json());
   app.use('/msgs', msgRouter);
